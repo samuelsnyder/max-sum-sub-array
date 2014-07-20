b9b2f1a53639a3bcd31d6302c44e682d71177ae7
Sam Snyder <samuel.l.snyder@gmail.com>
2014-07-20 15:40:01 -0700

    deleted old commented-out code

--- mtesterForTesting.c
+++ mtesterForTesting.c
@@ -97,31 +97,5 @@ int main(){
 		printf("%d\t%f\t%d\t%c\n",i+1, clockTime, sum, result );
 	}
     
-    
-    /* stuff goes here */
-	/* old shit
-     int testArray[] = {-18, -47, -40, -43, -2, 48, 31, -24, 36, -49, 4, -29, -4, -39, 12, 24, 8, 40, 45, -17, 6, -11, -5, -30, -8, 25, -44, -9, -20, -50, -12, -32, 41, 10, -42, -15, 11, -38, 37, 21, 33, -22, 16, -41, -46, -33, -26, 7, -3, -28, 29, 20, 27, 3, 15, 49, 23, -1, 14, 32, -31, -13, -48, -14, 13, 39, 46, -35, -36, 0, 17, -27, -21, 28, -7, 44, -10, 34, -19, 47, 42, -34, 5, 26, -45, 35, 9, -25, 38, -37, -23, 22, -6, -16, 18, 43, 30, 2, 19, 1};
-     int length = 100;
-     int sum = 0;
-     sum = divAndConMaxSubarray(testArray, 0, length);
-     
-     
-     end = clock();
-     
-     clockTime = ((double) (end - start))/CLOCKS_PER_SEC;
-     
-     printf("sum = %d\n",sum);
-     printf("clock time %d", clockTime);
-     */ 
-    
-	//
-	//loop to create giant array
-    
-    
-    
-    
-    
-    
-    
 	return 0;
 }
